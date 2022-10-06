c2ed6b1d63204ee20ff4d1026924bef7e18f498e
MuhammadYudaPratama <89918635+MuhammadYudaPratama@users.noreply.github.com>
2022-10-06 22:37:41 +0700

    Add files via upload

---
+++ PraktikumRelasi/src/PraktikumRelasi/Titik.java
@@ -0,0 +1,32 @@
+package PraktikumRelasi;
+
+/**
+ *
+ * @author YudaPratam
+ */
+public class Titik {
+    private double x;
+  private double y;
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public Titik(double X, double Y) {
+    x = X;
+    y = Y;
+  }
+
+  public void tampil() {
+    System.out.println("Point\t\t\t: [" + x + ", " + y + "]");
+  }
+
+  public double hitungJarak(Titik t2) {
+    return Math.sqrt(Math.pow(t2.getX() - x, 2) + Math.pow(t2.getY() - y, 2));
+  }
+
+}
---
+++ PraktikumRelasi/src/PraktikumRelasi/mainLimas.java
@@ -0,0 +1,19 @@
+import PraktikumRelasi.Limas;
+/**
+ *
+ * @author YudaPratam
+ */
+public class mainLimas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+         Limas limas = new Limas(1, 2, 5, 2, 2, 3, 2, 7);
+        limas.tampil();
+    }
+}
+
+    
+    
+
