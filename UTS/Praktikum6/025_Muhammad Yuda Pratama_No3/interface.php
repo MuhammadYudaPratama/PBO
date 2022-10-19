96e98859d84e615fe0912f8ad1aaf46496597730
MuhammadYudaPratama <89918635+MuhammadYudaPratama@users.noreply.github.com>
2022-10-19 23:51:08 +0700

    Add files via upload

---
+++ UTS/Praktikum6/025_Muhammad Yuda Pratama_No3/interface.php
@@ -0,0 +1,14 @@
+<!-- Muhammad Yuda Pratama - 21091397025 -->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
